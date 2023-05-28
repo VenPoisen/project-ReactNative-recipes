4a2aabfd44b2265d5393dc3b6cc1e5c1e2f84620
Alan Martins <alanmf.pro@gmail.com>
2023-05-28 14:39:34 -0300

    style: update index

--- app/index.js
+++ app/index.js
@@ -1,19 +1,18 @@
 import 'react-native-gesture-handler';
 import { useEffect, useState, } from 'react';
-import { View, Text, Image, FlatList, TouchableOpacity, SafeAreaView, ScrollView, ActivityIndicator, StatusBar } from 'react-native';
-import { Stack, useRouter } from 'expo-router';
+import { View, Text, Image, FlatList, TouchableOpacity, SafeAreaView, ScrollView, ActivityIndicator, StatusBar, Button } from 'react-native';
+import { useRouter } from 'expo-router';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faUser, faCalendarAlt, faLayerGroup, faStopwatch, faPizzaSlice, faEye } from '@fortawesome/free-solid-svg-icons';
 
-import { COLORS, SHADOWS } from '../constants/theme';
+import { COLORS, SHADOWS, FONT, SIZES } from '../constants/theme';
 import useFetch from '../hook/useFetch';
 import foodImage from "../assets/images/food.jpg";
 import styles from '../components/index.styles';
 
-const HomeScreen = function () {
+const HomeScreen = () => {
     const router = useRouter();
-
 
     const renderRecipeCard = ({ item }) => (
         <TouchableOpacity onPress={() => handleRecipePress(item.id)}>
@@ -25,18 +24,96 @@ const HomeScreen = function () {
         </TouchableOpacity>
     );
 
-    const handleRecipePress = (recipeId) => {
+    const handleRecipeDetail = (recipeId) => {
         // Navegar para a tela de detalhes da receita com o ID fornecido
-        // Implemente a navegação com a biblioteca de roteamento que você estiver usando (ex: React Navigation)
+        router.push(`/recipeDetail/${recipeId}`)
     };
-
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
-
             <StatusBar barStyle="light-content" backgroundColor={COLORS.white} />
 
             <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={SHADOWS.medium} id='1'>
+                    <View style={styles.cardContainer}>
+                        <TouchableOpacity>
+                            <Image source={foodImage} style={styles.recipeImage} />
+                        </TouchableOpacity>
+                        <View style={styles.recipeDescriptionContainer}>
+                            <Text style={styles.recipeTitle}>Big Poke Bowl</Text>
+                            <View style={styles.recipeAuthorContainer}>
+                                <TouchableOpacity>
+                                    <Text style={styles.recipeLinkableInfoText}>
+                                        <FontAwesomeIcon icon={faUser} style={styles.recipeLinkableInfoText} /> alanmartins
+                                    </Text>
+                                </TouchableOpacity>
+                                <Text style={styles.recipeDateText}>
+                                    <FontAwesomeIcon icon={faCalendarAlt} style={styles.recipeDateText} /> 13/05/2023 at 17:51
+                                </Text>
+                                <TouchableOpacity>
+                                    <Text style={styles.recipeLinkableInfoText}>
+                                        <FontAwesomeIcon icon={faLayerGroup} style={styles.recipeLinkableInfoText} /> salads
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                            <Text>This is the description of this recipe This is the description of this recipe This is the description of this recipe This is the description of this recipe</Text>
+                        </View>
+                        <View style={styles.recipeInfoContainer}>
+                            <View>
+                                <Text style={styles.recipeInfoTextTitle}><FontAwesomeIcon icon={faStopwatch} style={styles.recipeInfoTextTitle} /> Preparation</Text>
+                                <Text style={styles.recipeInfoText}>25 minutes</Text>
+                            </View>
+                            <View>
+                                <Text style={styles.recipeInfoTextTitle}><FontAwesomeIcon icon={faPizzaSlice} style={styles.recipeInfoTextTitle} /> Servings</Text>
+                                <Text style={styles.recipeInfoText}>4 People</Text>
+                            </View>
+                        </View>
+                        <TouchableOpacity style={styles.readMoreButton} onPress={handleRecipeDetail}>
+                            <Text style={styles.readMoreButtonText}><FontAwesomeIcon icon={faEye} style={styles.readMoreButtonText} /> Read More...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={SHADOWS.medium}>
+                    <View style={styles.cardContainer}>
+                        <TouchableOpacity>
+                            <Image source={foodImage} style={styles.recipeImage} />
+                        </TouchableOpacity>
+                        <View style={styles.recipeDescriptionContainer}>
+                            <Text style={styles.recipeTitle}>Big Poke Bowl</Text>
+                            <View style={styles.recipeAuthorContainer}>
+                                <TouchableOpacity>
+                                    <Text style={styles.recipeLinkableInfoText}>
+                                        <FontAwesomeIcon icon={faUser} style={styles.recipeLinkableInfoText} /> alanmartins
+                                    </Text>
+                                </TouchableOpacity>
+                                <Text style={styles.recipeDateText}>
+                                    <FontAwesomeIcon icon={faCalendarAlt} style={styles.recipeDateText} /> 13/05/2023 at 17:51
+                                </Text>
+                                <TouchableOpacity>
+                                    <Text style={styles.recipeLinkableInfoText}>
+                                        <FontAwesomeIcon icon={faLayerGroup} style={styles.recipeLinkableInfoText} /> salads
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                            <Text>This is the description of this recipe This is the description of this recipe This is the description of this recipe This is the description of this recipe</Text>
+                        </View>
+                        <View style={styles.recipeInfoContainer}>
+                            <View>
+                                <Text style={styles.recipeInfoTextTitle}><FontAwesomeIcon icon={faStopwatch} style={styles.recipeInfoTextTitle} /> Preparation</Text>
+                                <Text style={styles.recipeInfoText}>25 minutes</Text>
+                            </View>
+                            <View>
+                                <Text style={styles.recipeInfoTextTitle}><FontAwesomeIcon icon={faPizzaSlice} style={styles.recipeInfoTextTitle} /> Servings</Text>
+                                <Text style={styles.recipeInfoText}>4 People</Text>
+                            </View>
+                        </View>
+                        <TouchableOpacity style={styles.readMoreButton} onPress={handleRecipeDetail}>
+                            <Text style={styles.readMoreButtonText}><FontAwesomeIcon icon={faEye} style={styles.readMoreButtonText} /> Read More...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
                 <View style={SHADOWS.medium}>
                     <View style={styles.cardContainer}>
                         <TouchableOpacity>
@@ -76,45 +153,7 @@ const HomeScreen = function () {
                         </TouchableOpacity>
                     </View>
                 </View>
-                <View style={SHADOWS.medium}>
-                    <View style={styles.cardContainer}>
-                        <TouchableOpacity>
-                            <Image source={foodImage} style={styles.recipeImage} />
-                        </TouchableOpacity>
-                        <View style={styles.recipeDescriptionContainer}>
-                            <Text style={styles.recipeTitle}>Big Poke Bowl</Text>
-                            <View style={styles.recipeAuthorContainer}>
-                                <TouchableOpacity>
-                                    <Text style={styles.recipeLinkableInfoText}>
-                                        <FontAwesomeIcon icon={faUser} style={styles.recipeLinkableInfoText} /> alanmartins
-                                    </Text>
-                                </TouchableOpacity>
-                                <Text style={styles.recipeDateText}>
-                                    <FontAwesomeIcon icon={faCalendarAlt} style={styles.recipeDateText} /> 13/05/2023 at 17:51
-                                </Text>
-                                <TouchableOpacity>
-                                    <Text style={styles.recipeLinkableInfoText}>
-                                        <FontAwesomeIcon icon={faLayerGroup} style={styles.recipeLinkableInfoText} /> salads
-                                    </Text>
-                                </TouchableOpacity>
-                            </View>
-                            <Text>This is the description of this recipe This is the description of this recipe This is the description of this recipe This is the description of this recipe</Text>
-                        </View>
-                        <View style={styles.recipeInfoContainer}>
-                            <View>
-                                <Text style={styles.recipeInfoTextTitle}><FontAwesomeIcon icon={faStopwatch} style={styles.recipeInfoTextTitle} /> Preparation</Text>
-                                <Text style={styles.recipeInfoText}>25 minutes</Text>
-                            </View>
-                            <View>
-                                <Text style={styles.recipeInfoTextTitle}><FontAwesomeIcon icon={faPizzaSlice} style={styles.recipeInfoTextTitle} /> Servings</Text>
-                                <Text style={styles.recipeInfoText}>4 People</Text>
-                            </View>
-                        </View>
-                        <TouchableOpacity style={styles.readMoreButton}>
-                            <Text style={styles.readMoreButtonText}><FontAwesomeIcon icon={faEye} style={styles.readMoreButtonText} /> Read More...</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+
             </ScrollView>
         </SafeAreaView >
     );
